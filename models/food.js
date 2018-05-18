40835e659313599b05cf063fb07b681aa83c6c38
shre2332 <kurt.shreckengast@gmail.com>
2018-05-17 21:41:26 -0500

    new front end look and food schema updated

--- models/food.js
+++ models/food.js
@@ -42,6 +42,146 @@ var FoodSchema = new mongoose.Schema({
   Custom_Creator_ID: {
     type: String,//should be object id
     default: null
+  },
+  
+  //food label data
+  Serving_Size: {
+    type: Number
+  },
+  Calories_From_Fat: {
+    type: Number
+  },
+  Serving_Size: {
+    type: Number
+  },
+  Saturated_Fat: {
+    type: Number
+  },
+  Trans_Fat: {
+    type: Number
+  },
+  Cholesterol: {
+    type: Number
+  },
+  Sodium: {
+    type: Number
+  },
+  Sugars: {
+    type: Number
+  },
+  
+  //major minerals
+  Calcium: {
+    type: Number
+  },
+  Chloride: {
+    type: Number
+  },
+  Magnesium: {
+    type: Number
+  },
+  Phosphorus: {
+    type: Number
+  },
+  Potasium: {
+    type: Number
+  },
+  Sulfur: {
+    type: Number
+  },
+  
+  //trace minerals
+  Boron: {
+    type: Number
+  },
+  Cobalt: {
+    type: Number
+  },
+  Chlorine: {
+    type: Number
+  },
+  Chromium: {
+    type: Number
+  },
+  Copper: {
+    type: Number
+  },
+  Fluoride: {
+    type: Number
+  },                                 
+  Iodine: {
+    type: Number
+  },
+  Iron: {
+    type: Number
+  },
+  Lithium: {
+    type: Number
+  },
+  Manganese: {
+    type: Number
+  },
+  Molybdenum: {
+    type: Number
+  },
+  Selenium: {
+    type: Number
+  },
+  Sodium: {
+    type: Number
+  },
+  Zinc: {
+    type: Number
+  },
+                                     
+  //vitamins                                 
+  B1_Thiamin: {
+    type: Number
+  },
+  B2_Riboflavin: {
+    type: Number
+  },
+  B3_Niacin: {
+    type: Number
+  },
+  B5_Pantothenic_Acid: {
+    type: Number
+  },
+  B6_Group: {
+    type: Number
+  },
+  B7_Biotin: {
+    type: Number
+  },
+  B8_Ergadenylic_Acid: {
+    type: Number
+  },
+  B9_Folic_Acid: {
+    type: Number
+  },
+  B12_Cyanocobalamin: {
+    type: Number
+  },
+  Choline: {
+    type: Number
+  },
+  A_Retinol: {
+    type: Number
+  },
+  C_Absorbic_Acid: {
+    type: Number
+  },
+  D: {
+    type: Number
+  },
+  E_Tocopherol: {
+    type: Number
+  },
+  K: {
+    type: Number
+  },
+  Carotenoids: {
+    type: Number
   }
 
 });
